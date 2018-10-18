ab9f4ce93049e2d4c174e06c6ce318b1f490cb44
Jason <jasonharriot@gmail.com>
2018-10-18 13:03:53 -0400

    misc blacklisting

--- bozoid.js
+++ bozoid.js
@@ -11,7 +11,7 @@ const blacklistPath = "private/blacklist.json";
 var bozoid = JSON.parse(fs.readFileSync(configPath));
 var token = JSON.parse(fs.readFileSync(tokenPath)).token;
 
-var vocabulary;
+var vocabulary;	//lists are held in runtime and written to disk as they're updated
 try{
 	vocabulary = JSON.parse(fs.readFileSync(vocabularyPath));
 } catch(e){
@@ -29,9 +29,9 @@ try{
 	blacklist = {
 		users: [
 			{
-				id: "0",
-				by: "nobody",
-				reason: "void"
+				id: "420",
+				by: "Herobrine",
+				reason: "Griefing"
 			}
 		]
 	};
@@ -47,210 +47,210 @@ client.on('message', msg => {
 	
 	//
 
-	if(isCmd(msg.content, 0, "ping")){
-		msg.channel.send("Pong! Uptime: `" + (process.uptime() + "").toHHMMSS() + "`");
-	}
-
-	//
-
 	if(isCmd(msg.content, 0, "help")){
 		msg.channel.send("https://www.github.com/Montiey/BozoidJS\nI'm BozoidJS, Bozoid.java's younger, slightly stuipider cousin.");
-	}	
-	
-	//
-	
-	if(isCmd(msg.content, 0, "spam") && getArg(msg.content, 1) != null && !msg.author.bot){
-		msg.delete(0);
-		var limit = getArg(msg.content, 1);
-		limit = Math.min(limit, bozoid.spamLimit);
-		
-		
-		for(var i = 0; i < limit; i++){
-			msg.channel.send(getArg(msg.content, 2));
-		}
 	}
-	
-	//
-	
-	if(isCmd(msg.content, 0, "say") && getArg(msg.content, 1) != null  &&  !msg.author.bot){
-		msg.delete(0);
-		msg.channel.send(getArgs(msg.content, 1));
-	}
-	
-	if(isCmd(msg.content, 0, "zalgo") && getArg(msg.content, 1) != null  &&  !msg.author.bot){
-		msg.delete(0);
-		msg.channel.send(zalgo(getArgs(msg.content, 1)));
-	}
-	
-	if(isCmd(msg.content, 0, "emote") && getArg(msg.content, 1) != null && !msg.author.bot){
-		msg.delete(0);
-		var str = getArgs(msg.content, 1).toLowerCase().match(/[a-z0-9]/g).join("");
-		
-		var oStr = "";
-		var tempNew = "";
-		for(var i = 0; i < str.length; i++){
-			if(tempNew.length <= 2000) oStr = tempNew;
-			
-			var char = str.charAt(i);
-			
-			if(char == "b"){
-				tempNew = oStr + ":b:";
-				continue;
-			}
-			if(!isNaN(char)){
-				tempNew = oStr + ":" + numberConverter.toWords(parseInt(char)) + ":";
-				continue;
-			}
-			if(isNaN(char)){
-				tempNew = oStr + ":regional_indicator_" + char + ":";
-				continue;
-			}
-		}
-		if(tempNew.length <= 2000) oStr = tempNew;
-		
-		if(oStr != ""){
-			msg.channel.send(oStr);
-		}
-	}
-	
-	//
-	
-	if(!msg.author.bot){
-		for(var aka of bozoid.names){
-			if(msg.content.toLowerCase().includes(aka.toLowerCase())){
-				msg.channel.send(vocabulary.list[Math.floor(Math.random() * vocabulary.list.length)]);
-				break;
-			}
-		}
-	}
-	
-	//
-	
-	if(isCmd(msg.content, 0, "add") && !msg.author.bot && getArgs(msg.content, 1) != null && isMaster(msg)){
-		var word = getArgs(msg.content, 1);
-
-		if(vocabulary.list.indexOf(word) == -1){
-			vocabulary.list.push(word);
-			writeJSON(vocabulary, vocabularyPath);
-			msg.channel.send("Added to vocabulary: " + word);
-		} else{
-			console.log("Phrase already exists");
-		}
-	}
-	
-	if(isCmd(msg.content, 0, "remove") && !msg.author.but && getArgs(msg.content, 1) != null && isMaster(msg)){
-		var word = getArgs(msg.content, 1);
-		var index = 0;
-		for(var value of vocabulary.list){
-			if(value == word){
-				vocabulary.list.splice(index, 1);
-				writeJSON(vocabulary, vocabularyPath);
-				msg.channel.send("Removed: " + word);
-				break;
-			}
-			index++;
-		};
-	}
-	
-	//
-	if(msg.content.startsWith(".r")) msg.delete(0);
-	if(msg.content.includes("`No results found on`") || msg.content.startsWith("`Scor")) msg.delete(30000);
-
-	//
-
 	if((msg.content.toLowerCase().includes("nou") || msg.content.toLowerCase().includes("no u")) && !msg.author.bot){
 		msg.channel.send("no u");
 	}
-	
+
 	if(msg.content.toLowerCase().includes("gay") && !msg.author.bot){
 		msg.channel.send("you have the big gay");
 	}
 	
-	if(isCmd(msg.content, 0, "restart") || isCmd(msg.content, 0, "reboot")){
-		setStatus(bozoid.game, "offline");
-		msg.channel.send("Restarting...");
-		process.exit(0);
-	}
-	
-	if(isCmd(msg.content, 0, "shutdown")){
-		setStatus(bozoid.game, "offline");
-		msg.channel.send("Shutting Down...");
-		process.exit(1);
-	}
-	
-	//
-	
-	if(isCmd(msg.content, 0, "blacklist") && getArgs(msg.content, 1) == null){
-		var oStr = "";
-		for(var listed of blacklist.users){
-			var targetMember = msg.guild.member(listed.id);
-			
-			if(targetMember == null) continue;
-			
-			var tempNew = "`" + targetMember.user.username + "#" + targetMember.user.discriminator + " (" + targetMember.user.id + ")`\n";
-			
-			if((oStr + tempNew).length > 2000){
-				msg.channel.send(oStr);
-				oStr = tempNew;
-			} else{
-				oStr += tempNew;
-			}
-		}
-		
-		if(oStr.length > 0) msg.channel.send(oStr);
-		else msg.channel.send("Everyone is nice :)");
-		
-	}
-	
-	if(isCmd(msg.content, 0, "blacklist") && getArgs(msg.content, 1) != null && isMaster(msg)){
-		var targetMember = msg.guild.member(getArg(msg.content, 1).substr(2, 18));	//Discord snowflakes are 18 characters long
-		
-		console.log("id: " + targetMember.id + " of " + targetMember.user.username);
-		
-		if(targetMember != null){
-			var id = targetMember.id;
-			var by = msg.author.id;
-			var reason = getArgs(msg.content, 2);
-			
-			var exists = false;
-			for(var listed of blacklist.users){
-				if(listed.id == id){
-					exists = true;
+	if(!msg.author.bot){
+			for(var aka of bozoid.names){
+				if(msg.content.toLowerCase().includes(aka.toLowerCase())){
+					msg.channel.send(vocabulary.list[Math.floor(Math.random() * vocabulary.list.length)]);
+					break;
 				}
 			}
-			
-			if(!exists){
-				blacklist.users.push({
-					id: id,
-					by: by,
-					reason: reason
-				});
+		}
+	
+	////////
+
+	if(!isBlacklisted(msg.author)){	//Commands here vvv
+		
+	   if(isCmd(msg.content, 0, "ping")){
+			msg.channel.send("Pong! Uptime: `" + (process.uptime() + "").toHHMMSS() + "`");
+	   }
+
+		//
+
+		if(isCmd(msg.content, 0, "spam") && getArg(msg.content, 1) != null && !msg.author.bot){
+			msg.delete(0);
+			var limit = getArg(msg.content, 1);
+			limit = Math.min(limit, bozoid.spamLimit);
+
+
+			for(var i = 0; i < limit; i++){
+				msg.channel.send(getArg(msg.content, 2));
+			}
+		}
+
+		//
+
+		if(isCmd(msg.content, 0, "say") && getArg(msg.content, 1) != null  &&  !msg.author.bot){
+			msg.delete(0);
+			msg.channel.send(getArgs(msg.content, 1));
+		}
+
+		if(isCmd(msg.content, 0, "zalgo") && getArg(msg.content, 1) != null  &&  !msg.author.bot){
+			msg.delete(0);
+			msg.channel.send(zalgo(getArgs(msg.content, 1)));
+		}
+
+		if(isCmd(msg.content, 0, "emote") && getArg(msg.content, 1) != null && !msg.author.bot){
+			msg.delete(0);
+			var str = getArgs(msg.content, 1).toLowerCase().match(/[a-z0-9]/g).join("");
+
+			var oStr = "";
+			var tempNew = "";
+			for(var i = 0; i < str.length; i++){
+				if(tempNew.length <= 2000) oStr = tempNew;
+
+				var char = str.charAt(i);
+
+				if(char == "b"){
+					tempNew = oStr + ":b:";
+					continue;
+				}
+				if(!isNaN(char)){
+					tempNew = oStr + ":" + numberConverter.toWords(parseInt(char)) + ":";
+					continue;
+				}
+				if(isNaN(char)){
+					tempNew = oStr + ":regional_indicator_" + char + ":";
+					continue;
+				}
+			}
+			if(tempNew.length <= 2000) oStr = tempNew;
+
+			if(oStr != ""){
+				msg.channel.send(oStr);
+			}
+		}
+
+		//
+
+		if(isCmd(msg.content, 0, "add") && !msg.author.bot && getArgs(msg.content, 1) != null && isMaster(msg)){
+			var word = getArgs(msg.content, 1);
+
+			if(vocabulary.list.indexOf(word) == -1){
+				vocabulary.list.push(word);
+				writeJSON(vocabulary, vocabularyPath);
+				msg.channel.send("Added to vocabulary: " + word);
+			} else{
+				console.log("Phrase already exists");
+			}
+		}
+
+		if(isCmd(msg.content, 0, "remove") && !msg.author.but && getArgs(msg.content, 1) != null && isMaster(msg)){
+			var word = getArgs(msg.content, 1);
+			var index = 0;
+			for(var value of vocabulary.list){
+				if(value == word){
+					vocabulary.list.splice(index, 1);
+					writeJSON(vocabulary, vocabularyPath);
+					msg.channel.send("Removed: " + word);
+					break;
+				}
+				index++;
+			};
+		}
+
+		//
+		if(msg.content.startsWith(".r")) msg.delete(0);
+		if(msg.content.includes("`No results found on`") || msg.content.startsWith("`Scor")) msg.delete(30000);
+
+		//
+
+		if(isCmd(msg.content, 0, "restart") || isCmd(msg.content, 0, "reboot")){
+			setStatus(bozoid.game, "offline");
+			msg.channel.send("Restarting...");
+			process.exit(0);
+		}
+
+		if(isCmd(msg.content, 0, "shutdown")){
+			setStatus(bozoid.game, "offline");
+			msg.channel.send("Shutting Down...");
+			process.exit(6969420);
+		}
+
+		//
+
+		if(isCmd(msg.content, 0, "blacklist") && getArgs(msg.content, 1) == null){
+			var oStr = "Blacklisted users: \n";
+			for(var listed of blacklist.users){
+				var targetMember = msg.guild.member(listed.id);
+
+				if(targetMember == null) continue;
+
+				var tempNew = "`" + targetMember.user.username + "#" + targetMember.user.discriminator + " (" + targetMember.user.id + ")`\n";
+
+				if((oStr + tempNew).length > 2000){
+					msg.channel.send(oStr);
+					oStr = tempNew;
+				} else{
+					oStr += tempNew;
+				}
+			}
+
+			if(oStr.length > 0) msg.channel.send(oStr);
+			else msg.channel.send("Everyone is nice :)");
+
+		}
+
+		if(isCmd(msg.content, 0, "blacklist") && getArgs(msg.content, 1) != null && isMaster(msg)){
+			var targetMember = msg.guild.member(getArg(msg.content, 1).substr(2, 18));	//Discord snowflakes are 18 characters long
+
+			console.log("id: " + targetMember.id + " of " + targetMember.user.username);
+
+			if(targetMember != null){
+				var id = targetMember.id;
+				var by = msg.author.id;
+				var reason = getArgs(msg.content, 2);
+
+				var exists = false;
+				for(var listed of blacklist.users){
+					if(listed.id == id){
+						exists = true;
+					}
+				}
+
+				if(!exists){
+					blacklist.users.push({
+						id: id,
+						by: by,
+						reason: reason
+					});
+
+					writeJSON(blacklist, blacklistPath);
+
+					msg.channel.send("`" + targetMember.user.username + "` may no longer use `" + bozoid.cmdPref + "` commands.\nReason: `" + reason + "`");
+				} else{
+					msg.channel.send("`" + targetMember.user.username + "` has already been blacklisted");
+				}
+			}
+		}
+
+		if(isCmd(msg.content, 0, "unblacklist") && isMaster(msg)){
+			var targetMember = msg.guild.member(getArg(msg.content, 1).substr(2, 18));	//Discord snowflakes are 18 characters long
+
+			console.log("id: " + targetMember.id + " of " + targetMember.user.username);
+
+			if(targetMember != null){
+				var id = targetMember.id;
+
+				for(var listed of blacklist.users){
+					if(listed.id === id){
+						blacklist.users.splice(blacklist.users.indexOf(listed), 1);
+						msg.channel.send("`" + targetMember.user.username + "` was removed from the blacklist");
+					}
+				}
 
 				writeJSON(blacklist, blacklistPath);
-
-				msg.channel.send("`" + targetMember.user.username + "` may no longer use `" + bozoid.cmdPref + "` commands.\nReason: `" + reason + "`");
-			} else{
-				msg.channel.send("`" + targetMember.user.username + "` has already been blacklisted");
 			}
-		}
-	}
-	
-	if(isCmd(msg.content, 0, "unblacklist") && isMaster(msg)){
-		var targetMember = msg.guild.member(getArg(msg.content, 1).substr(2, 18));	//Discord snowflakes are 18 characters long
-		
-		console.log("id: " + targetMember.id + " of " + targetMember.user.username);
-		
-		if(targetMember != null){
-			var id = targetMember.id;
-			
-			for(var listed of blacklist.users){
-				if(listed.id === id){
-					blacklist.users.splice(blacklist.users.indexOf(listed), 1);
-					msg.channel.send("`" + targetMember.user.username + "` was removed from the blacklist");
-				}
-			}
-			
-			writeJSON(blacklist, blacklistPath);
 		}
 	}
 	
@@ -264,6 +264,13 @@ client.on('error', e => {
 client.login(token);
 
 ////////////////
+
+function isBlacklisted(user){
+	for(var listed of blacklist){
+		if(listed.id == user.id) return true;
+	}
+	return false;
+}
 
 function writeJSON(obj, path){	//Careful! Keep production .jsons safe from untested write operations!
 	var text = JSON.stringify(obj, null, 4);
