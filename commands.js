dee0b2cdefb7d7adf82119e3423237c8349c9325
Jason <jasonharriot@gmail.com>
2018-11-07 15:49:37 -0500

    (workspace change)

---
+++ commands.js
@@ -0,0 +1,40 @@
+var commands = [
+	{
+		"context": "message",	//What kind of thing do I have to use to do the stuff that I have to do with?
+		"parameters": [	//Ordered list (e.g. $parameter0 parameter1 parameter2)
+			{
+				"type": "command",
+				"text": "ping",
+				"caseSensitive": false
+			}
+		],
+		"script": function(command, parameters, message){	//The command (as written here) gets passed to the function (also written here) once it's parsed
+			message.channel.send("Pong!");
+		}
+	},
+	{
+		"context": "message",
+		"parameters": [
+
+		]
+	}
+	{
+		"context": "message",
+		"parameters": [
+			{
+				"type": "command",
+				"text": "say",
+				"caseSensitive": false
+			},
+			{
+				"type": "input"
+			}
+		],
+		"script": function(command, parameters, message){
+			message.channel.send("Someone said " + command.parameter);
+			message.channel.send("Now i'll say the second parameter: " + parameters[1]);
+			message.channel.send("Now i'll delete the message");
+			message.delete(0);
+		}
+	}
+];
