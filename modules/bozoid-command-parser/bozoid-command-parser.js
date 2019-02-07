0b8eff4f1a23f41d0c649f954c6b28274d7b0b76
Montiey <jasonharriot@gmail.com>
2019-02-06 22:22:31 -0500

    Added custom responder, tweaked command structure

--- modules/bozoid-command-parser/bozoid-command-parser.js
+++ modules/bozoid-command-parser/bozoid-command-parser.js
@@ -1,4 +1,6 @@
 const parser = require("bozoid-command-parser");
+const fileIO = require("bozoid-file-grabber");
+const bozoid = fileIO.read("bozoid.json");
 
 exports.isBlacklisted = function(user){
 	for(var listed of blacklist.users){
@@ -12,24 +14,34 @@ exports.isBlacklisted = function(user){
 
 exports.getArg = function(str, index){	//Returns the string of an argument at an index
 	var tmpStr = str;
+	var separator = "\"";
 	for(var i = 0; i < index; i++){
-		while(!tmpStr.startsWith(" ")){
+		while(!tmpStr.startsWith(separator)){
 			tmpStr = tmpStr.substring(1);
 
 			if(tmpStr.length <= 1){
+				console.log("No argument");
 				return null;
 			}
 		}
 		tmpStr = tmpStr.substring(1);
 	}
-	var EOA = tmpStr.indexOf(" ");	//End of argument index
+	var EOA = tmpStr.indexOf("\"");	//End of argument index
 	if(EOA >= 0){
 		var oStr = tmpStr.substring(0, EOA);	//Excludes next space if index is -1
-		if(oStr.length > 0) return oStr;
+		if(oStr.length > 0){
+			console.log("Got1: " + oStr);
+			return oStr;
+		}
+		console.log("No argument 1");
 		return null;
 	} else{	//if there are no spaces in the string
 		var oStr = tmpStr.substring(0, tmpStr.length);
-		if(oStr.length > 0) return oStr;
+		if(oStr.length > 0){
+			console.log("Got2: " + oStr);
+			return oStr;
+		}
+		console.log("No argument 2");
 		return null;
 	}
 }
@@ -39,25 +51,15 @@ exports.getRest = function(str, index){	//Returns the rest of a string at an arg
 	var oStr = "";
 	var gotArg = parser.getArg(str, tmpIndex);
 	while(gotArg != null){
-		oStr += " " + gotArg;
+		oStr += gotArg;
 		tmpIndex++;
 		gotArg = parser.getArg(str, tmpIndex);
 	}
 	oStr = oStr.substring(1);	//Because I added a lazy space
-	if(oStr.length > 0) return oStr;
-	return null
+	if(oStr.length > 0){
+		console.log("Rest of: " + oStr);
+		return oStr;
+	}
+	console.log("No rest of");
+	return null;
 }
-
-// exports.getArgList = function(str){
-// 	var list = [];
-// 	var i;
-// 	var toAdd;
-// 	while(toAdd != null){
-// 		toAdd = parser.getArg(str, i);
-// 		if(toAdd != null){
-// 			list.push(toAdd);
-// 			i++;
-// 		}
-// 	}
-// 	return list;
-// }
