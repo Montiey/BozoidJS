85e3550c82d33f04ac0efc5d538f7bbbf7e27322
Montiey <montieyd@ip-172-31-94-148.ec2.internal>
2020-06-24 01:29:06 +0000

    Added fricjar leaderboards

--- modules/bozoid-commands/bozoid-commands.js
+++ modules/bozoid-commands/bozoid-commands.js
@@ -584,6 +584,45 @@ exports.list = {
 			}
 		},
 		{
+			description: "Frick leaderboards",
+			allowBot: false,
+			command: "jartop",
+			script: function(cmd, msg){
+				let json = fileIO.read("frickjar.json");
+
+				let inA = json.list;
+				let outA = [];
+				let numTop = 8;
+
+				for(let i = 0; i < numTop; i++){
+					let found = null;
+					let foundV = 0;
+					let realName = "";
+					let succ = false;
+					for(let j = 0; j < inA.length; j++){
+						let member = msg.guild.member(inA[j].id);
+						if(inA[j].total > foundV && member != null){
+							found = j;
+							foundV = inA[j].total;
+							succ = true;
+						}
+					}
+					if(succ){
+						outA.push(inA.splice(found, 1)[0]);
+					}
+				}
+
+				let text = "Frick Jar™ top balances:```";
+				for(let entry of outA){
+					text += "$" + entry.total + " - " + entry.referenceName + "\n";
+				}
+
+				text += "```";
+
+				msg.channel.send(text);
+			}
+		},
+		{
 			description: "Log message statistics",
 			noHelp: true,
 			allowBot: true,
